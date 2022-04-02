0b40688dbb3a14977fdfc135b47dd336e9f31e8e
Kepova Valentina <v.kepova@gmail.com>
2022-04-02 12:52:03 +0500

    перенести поиск элементов картинки и названия в конструктор попапа с картинкой

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -2,8 +2,6 @@ export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
     this._handleEscClose = this._handleEscClose.bind(this);
-    this._popupImgSrc = this._popup.querySelector('.popup__img');
-    this._popupImgTitle = this._popup.querySelector('.popup__img-title');
   }
 
   open() {
--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -1,6 +1,12 @@
 import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
 
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._popupImgSrc = this._popup.querySelector('.popup__img');
+    this._popupImgTitle = this._popup.querySelector('.popup__img-title');
+  }
+
   open(data) {
     super.open();
     this._popupImgSrc.src = data.link;
